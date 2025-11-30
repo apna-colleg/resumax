3ef62750870ea89b9986c9eefc267ccd880263da
apna-colleg <hemashrivastava06@gmail.com>
2025-11-30 15:31:36 +0530

    Refactor Home component and remove Welcome page assets. Updated meta title and description for the Home page. Replaced Welcome component with a new layout for tracking applications and resume feedback. Deleted unused logo assets.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResuMax" },
+    { name: "description", content: "Smart Feedback for you dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main>
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Rating</h1>
+          <h2>Review your submissions and check AI-powered feedback</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
